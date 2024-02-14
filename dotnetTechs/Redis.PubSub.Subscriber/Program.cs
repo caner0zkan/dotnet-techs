dd7274c977b46762736d87e0d6af95ecbe94092d
caner0zkan <canerozkan513@gmail.com>
2024-02-14 22:10:58 +0300

    pub sub

---
+++ dotnetTechs/Redis.PubSub.Subscriber/Program.cs
@@ -0,0 +1,11 @@
+﻿using StackExchange.Redis;
+
+ConnectionMultiplexer conn = await ConnectionMultiplexer.ConnectAsync("localhost:1453");
+
+ISubscriber subscriber = conn.GetSubscriber();
+
+await subscriber.SubscribeAsync("myChannel", (channel, message) => {
+    Console.WriteLine(message);
+});
+
+Console.Read();
