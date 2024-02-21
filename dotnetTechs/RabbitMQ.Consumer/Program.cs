ed5d459b56cf2871149116a919bab059cd315755
caner0zkan <canerozkan513@gmail.com>
2024-02-21 22:59:11 +0300

    rabbitmq

---
+++ dotnetTechs/RabbitMQ.Consumer/Program.cs
@@ -0,0 +1,19 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+var factory = new ConnectionFactory();
+factory.Uri = new("amqps://jpdcfjkp:IW7jASj-pN11AA8KrZQ3yldqdWBYBt_o@sparrow.rmq.cloudamqp.com/jpdcfjkp");
+
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+channel.QueueDeclare(queue: "example-queue", exclusive: false);
+
+var consumer = new EventingBasicConsumer(channel);
+channel.BasicConsume(queue: "example-queue", false, consumer);
+consumer.Received += (sender, e) => {
+    Console.WriteLine(Encoding.UTF8.GetString(e.Body.Span));
+};
+
+Console.Read();
---
+++ dotnetTechs/RabbitMQ.Publisher/Program.cs
@@ -0,0 +1,16 @@
+﻿
+using RabbitMQ.Client;
+using System.Text;
+
+var factory = new ConnectionFactory();
+factory.Uri = new("amqps://jpdcfjkp:IW7jASj-pN11AA8KrZQ3yldqdWBYBt_o@sparrow.rmq.cloudamqp.com/jpdcfjkp");
+
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+channel.QueueDeclare(queue: "example-queue", exclusive: false);
+
+var message = Encoding.UTF8.GetBytes("Merhaba");
+channel.BasicPublish(exchange: "", routingKey: "example-queue", body: message);
+
+Console.Read();
